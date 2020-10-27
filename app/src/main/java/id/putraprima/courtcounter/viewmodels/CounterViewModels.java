fbf139cfc5ef23c7a637e27b2f02a0da971117c9
fachry1410 <fachrynajibb2000@gmail.com>
2020-10-27 13:05:09 +0700

    Implement reset point

--- app/src/main/java/id/putraprima/courtcounter/viewmodels/CounterViewModels.java
+++ app/src/main/java/id/putraprima/courtcounter/viewmodels/CounterViewModels.java
@@ -29,5 +29,13 @@ public class CounterViewModels extends ViewModel {
         Log.d("Point Away : ", String.valueOf(score.getAwayScore()));
     }
 
+    public void resetPoint(){
+        score.setAwayScore(0);
+        score.setHomeScore(0);
+        scoreMutableLiveData.setValue(score);
+        Log.d("Point Home : ", String.valueOf(score.getHomeScore()));
+        Log.d("Point Away : ", String.valueOf(score.getAwayScore()));
+    }
+
     //TODO : Tambahkan Implementasi View Model Untuk Reset Point
 }
